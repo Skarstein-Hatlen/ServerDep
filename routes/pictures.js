7e4261f03ab331411cbe850e97483ae830d9914a
Skarstein-Hatlen <kjetil.skarstein-hatlen@hotmail.no>
2023-05-03 11:26:08 +0200

    Cyc

--- routes/pictures.js
+++ routes/pictures.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const fs = require('fs');
 var path = require('path');
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
 
 // Or
 /* GET pictures listing. */
@@ -13,6 +15,17 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
     const file = req.files.file;
     fs.writeFileSync(path.join(__dirname, '../pictures/', file.name), file.data);
+    res.end();
+});
+
+router.post('/', async function(req, res, next) {
+    const file = req.files.file;
+    console.log(req.files);
+    await s3.putObject({
+        Body: file.data,
+        Bucket: process.env.CYCLIC_BUCKET_NAME,
+        Key: "public/" + file.name,
+    }).promise()
     res.end();
 });
 
